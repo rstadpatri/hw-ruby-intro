85ca2d35700c1ebf56a04ae2e7ed56b4e85857eb
rstadpatri <rstadpatri@gmail.com>
2019-09-16 21:45:57 -0400

    max_2_sum implemented

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -7,7 +7,10 @@ def sum arr
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  first_max = arr.max #find first max of arr
+  arr.delete_at(arr.index(first_max)) #delete first max froma arr
+  second_max = arr.max  #find new max
+  puts sum([first_max, second_max]) #call previously written sum function to add 2 highest elements
 end
 
 def sum_to_n? arr, n
@@ -34,4 +37,6 @@ class BookInStock
 # YOUR CODE HERE
 end
 
-puts sum([0,1,2]) #calls def sum
+#function calls
+#puts sum([0,1,2]) #calls def sum
+puts max_2_sum([1, 2, 3])
