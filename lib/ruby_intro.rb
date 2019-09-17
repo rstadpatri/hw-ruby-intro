010354536e2f4f829679169d97f5a6ef53d33c96
rstadpatri <rstadpatri@gmail.com>
2019-09-17 19:00:11 -0400

    implemented parts 1 and 2

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -7,6 +7,11 @@ def sum arr
 end
 
 def max_2_sum arr
+  if arr.length == 1 #error check for single element arr
+    return arr[0]
+  elsif arr.length == 0 #error check for empty arr
+    return 0
+  end
   first_max = arr.max #find first max of arr
   arr.delete_at(arr.index(first_max)) #delete first max froma arr
   second_max = arr.max  #find new max
@@ -17,24 +22,34 @@ def sum_to_n? arr, n
   if arr.length == 0 || arr.length == 1 #checks if arr is a single elemens
     return false
   else 
-    arr.combination(2).any? {|a, b| a + b == n} #checks every combination and sums them, if equal to n returns true, else returns false
+    arr.combination(2).any? {|a, b| a + b == n} #checks every combination and sums them, if equal to n returns true, else returns false (last line value returned as default)
   end
 end
-
 
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  "Hello, #{name}"
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  first_letter = s[0] #gets first letter of string s
+  true_array = ['a', 'e', 'i', 'o', 'u'] #arr of vowels
+  true_array.include? first_letter #check and return true if first letter is included in array
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  if s.length == 0 || s.count('01') != s.length
+    return false
+  end
+  new_s = s.delete(' ') #strip all white space
+  new_int = new_s.to_i(2) #convert binary string to integer base 2
+  if new_int % 4 == 0
+    return true
+  else
+    return false
+  end
 end
 
 # Part 3
@@ -43,7 +58,38 @@ class BookInStock
 # YOUR CODE HERE
 end
 
-#function calls
-#puts sum([0,1,2]) #calls def sum
+#FUNCTION CALLS
+
+#PART 1
+#SUM
+#puts sum([0,1,2]) 
+#puts sum([])
+
+#MAX 2 SUM  
 #puts max_2_sum([1, 2, 3])
-puts sum_to_n?([1, 4, 3], 6)
+#puts max_2_sum([1, 2, 3, 3])
+#puts max_2_sum([])
+#puts max_2_sum([3])
+
+#SUM TO N
+#puts sum_to_n?([1, 4, 3], 6)
+#puts sum_to_n?([-1, -2, 3, 4, 5, -8], -3)
+#puts sum_to_n?([-1], -1)
+#puts sum_to_n?([], 7)
+
+#PART 2
+#HELLO
+#puts hello('Dan') 
+
+#STARTS WITH CONSONANT
+#puts "'atest' (should return true): " + starts_with_consonant?('atest').to_s
+#puts "'test' (should return false): " + starts_with_consonant?('test').to_s
+#puts "'' (should return false): " + starts_with_consonant?('').to_s
+#puts "'#@$' (should return false): " + starts_with_consonant?('#@$').to_s
+
+#BINARY MULTIPLE OF 4
+#puts binary_multiple_of_4?('101 1000000000001')
+#puts binary_multiple_of_4?('0100')
+#puts binary_multiple_of_4?('a100')
+#puts binary_multiple_of_4?(' ')
+
